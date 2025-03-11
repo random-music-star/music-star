c3f9d3194b322dbd3808de5f361f9f9eb0d3c225
HoberMin <102784200+HoberMin@users.noreply.github.com>
2025-03-11 19:48:29 +0900

    chore: eslint 플러그인 수정

--- FE/src/pages/index.tsx
+++ FE/src/pages/index.tsx
@@ -1,7 +1,5 @@
-import Image from 'next/image';
 import { Geist, Geist_Mono } from 'next/font/google';
 
-const m = 1;
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
