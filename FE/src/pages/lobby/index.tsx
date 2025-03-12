3a0b14b3f07509c55ff0b5d2a30b727b2b17cd47
송하은 <she3469@gmail.com>
2025-03-12 15:53:30 +0900

    feat: 전체 채팅 토픽 구독 및 상태 관리

--- FE/src/pages/lobby/index.tsx
+++ FE/src/pages/lobby/index.tsx
@@ -1,7 +1,52 @@
+import { useEffect, useState } from 'react';
 import SocketLayout from '@/components/layouts/SocketLayout';
+import { useWebSocketStore } from '@/stores/useWebsocketStore';
 
 const LobbyPage = () => {
-  return <SocketLayout>lobby</SocketLayout>;
+  const { sendMessage, updateSubscription, isConnected, publicChattings } =
+    useWebSocketStore();
+  const [inputValue, setInputValue] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  useEffect(() => {
+    updateSubscription('channel');
+  }, [isConnected]);
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log('입력된 값:', inputValue);
+    sendMessage('/app/channel/1', {
+      type: 'chatting',
+      request: {
+        sender: 'hoberMin',
+        message: inputValue,
+      },
+    });
+
+    setInputValue('');
+  };
+
+  return (
+    <SocketLayout>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          value={inputValue}
+          onChange={handleChange}
+          placeholder='입력하세요'
+        />
+        <button type='submit'>전송</button>
+      </form>
+      <div>
+        {publicChattings.map(chat => (
+          <p>{`[${chat.messageType}] ${chat.sender}: ${chat.message}`}</p>
+        ))}
+      </div>
+    </SocketLayout>
+  );
 };
 
 export default LobbyPage;
