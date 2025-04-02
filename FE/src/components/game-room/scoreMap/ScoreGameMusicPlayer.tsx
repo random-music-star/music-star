c823039091a2d9d707f90e0f67346dafde99422f
손호민 <sonhomin98@naver.com>
2025-04-02 13:00:12 +0900

    Feat/gameroom error/son

--- FE/src/components/game-room/scoreMap/ScoreGameMusicPlayer.tsx
+++ FE/src/components/game-room/scoreMap/ScoreGameMusicPlayer.tsx
@@ -6,28 +6,33 @@ import ScoreRoundPlayContent from './ScoreRoundPlayContent';
 import ScoreRoundResultContent from './ScoreRoundResultContent';
 
 interface MusicPlayerProps {
-  gameState: 'GAME_RESULT' | 'ROUND_START';
+  gameState: 'GAME_RESULT' | 'ROUND_START' | 'SCORE_UPDATE';
 }
 
 const ScoreGameMusicPlayer = ({ gameState }: MusicPlayerProps) => {
   const { roundInfo } = useGameRoundInfoStore();
   if (!roundInfo) return null;
   const { round: currentRound } = roundInfo;
+
   return (
-    <div className='flex h-full w-full flex-col rounded-md border border-purple-200 bg-purple-50 p-4'>
+    <div className='flex h-full w-full flex-col p-4'>
       <div className='mb-4 flex items-center justify-between'>
-        <h2 className='text-2xl font-bold text-purple-800'>
-          라운드 {currentRound}
-        </h2>
-        <div className='text-base font-medium text-purple-700'>
+        <div className='flex items-center'>
+          <span className='rounded-md bg-white/10 px-3 py-1 text-xl font-bold text-white'>
+            라운드 {currentRound}
+          </span>
+        </div>
+
+        <div className='text-sm font-medium text-cyan-200'>
           {gameState === 'GAME_RESULT' ? (
-            <span>{"다음 문제를 풀려면 채팅에 '.' 입력"}</span>
+            <span>{"다음 문제: '.' 입력"}</span>
           ) : (
-            <span>{"정답 입력: 채팅창에 '.' 입력 (스킵: .)"}</span>
+            <span>{"정답/스킵: '.' 입력"}</span>
           )}
         </div>
       </div>
-      <div className='grow'>
+
+      <div className='grow rounded-md bg-white/5 p-3'>
         {gameState === 'ROUND_START' ? (
           <ScoreRoundPlayContent />
         ) : (
