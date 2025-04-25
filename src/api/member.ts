b7861a5d68a2e671bbc364dd44dbe2071a7a97c9
HoberMin <102784200+HoberMin@users.noreply.github.com>
2025-04-25 14:04:29 +0900

    feat: api 스펙 변경에 따른 코드 수정

--- src/api/member.ts
+++ src/api/member.ts
@@ -1,38 +1,15 @@
 import { type ApiContext, api } from './core';
 
-interface LoginForm {
+export interface Member {
   username: string;
-  password: string;
-}
-
-interface Member {
-  username: string;
-  memberType: string;
+  memberType: 'GUEST' | 'USER';
   inLobby: boolean;
-  roomStatus: null | 'WAITING' | 'IN_PROGRESS' | 'END';
+  roomStatus: 'WAITING' | 'IN_PROGRESS' | null;
 }
 
 export const userNicknameAPI = (ctx?: ApiContext) =>
-  api<{ nickname: string }>('/member/nickname', { method: 'GET' }, ctx);
+  api<{ username: string }>('/auth/me', { method: 'GET' }, ctx);
 
-export const guestLoginAPI = (ctx?: ApiContext) =>
-  api<{ token: string }>('/member/guest/login', { method: 'GET' }, ctx);
-
-export const userLoginAPI = (loginForm: LoginForm, ctx?: ApiContext) =>
-  api<{ token: string }>(
-    '/member/user/login',
-    { method: 'POST', body: JSON.stringify(loginForm) },
-    ctx,
-  );
-
-export const userSignupAPI = (loginForm: LoginForm, ctx?: ApiContext) =>
-  api<{ token: string }>(
-    '/member/user/signup',
-    { method: 'POST', body: JSON.stringify(loginForm) },
-    ctx,
-  );
-
-// 채널 내 유저 목록 조회
 export const getChannelUsersAPI = (channelId: string, ctx?: ApiContext) =>
   api<{ users: Member[] }>(
     `/member/${channelId}`,
