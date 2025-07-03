7531ac05b70fa7d22e4a0b1acd08be5340d32ebf
songhaeunsong <she3469@gmail.com>
2025-07-03 22:49:24 +0900

    fix: RotatingLP 불필요한 상태 제

--- src/components/game-room/gamePlaySection/RotatingLP.tsx
+++ src/components/game-room/gamePlaySection/RotatingLP.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 import { motion, useAnimation } from 'framer-motion';
 import Image from 'next/image';
@@ -7,15 +7,12 @@ import { useGameStateStore } from '@/stores/websocket/useGameStateStore';
 
 const RotatingLP = () => {
   const { gameState } = useGameStateStore();
-
-  const [currentAngle, setCurrentAngle] = useState<number>(0);
   const controls = useAnimation();
-  let animationInterval: number | null = null;
 
   useEffect(() => {
     if (gameState !== 'ROUND_INFO' && gameState !== 'ROUND_OPEN') {
       controls.start({
-        rotate: [currentAngle, currentAngle + 360],
+        rotate: [0, 360],
         opacity: 1,
         transition: {
           repeat: Infinity,
@@ -23,38 +20,15 @@ const RotatingLP = () => {
           ease: 'linear',
         },
       });
-
-      animationInterval = window.setInterval(() => {
-        setCurrentAngle(prev => (prev + 2) % 360);
-      }, 10);
     } else {
-      if (animationInterval !== null) {
-        clearInterval(animationInterval);
-        animationInterval = null;
-      }
-
-      controls.start({
-        rotate: currentAngle,
-        opacity: 1,
-        transition: {
-          duration: 0,
-        },
-      });
+      controls.stop();
     }
-
-    return () => {
-      if (animationInterval !== null) {
-        clearInterval(animationInterval);
-        animationInterval = null;
-      }
-    };
-  }, [gameState]);
+  }, [gameState, controls]);
 
   return (
     <motion.div
       className='absolute -left-40 h-[300px] w-[300px] -translate-y-8 overflow-hidden rounded-full lg:-left-38 lg:-translate-y-12'
       animate={controls}
-      initial={{ rotate: currentAngle }}
     >
       <div className='h-full w-full rounded-full'>
         <Image src='/lp.svg' alt='lp' fill className='object-cover' />
